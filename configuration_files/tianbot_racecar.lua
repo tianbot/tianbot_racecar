8d5ad20b5e5cd5fe0510f340d594a628c648821e
Tian Bo <tianb03@hotmail.com>
2018-09-19 19:55:02 +0800

    lua update

--- configuration_files/tianbot_racecar.lua
+++ configuration_files/tianbot_racecar.lua
@@ -46,7 +46,7 @@ MAP_BUILDER.use_trajectory_builder_2d = true
 
 TRAJECTORY_BUILDER_2D.submaps.num_range_data = 35
 TRAJECTORY_BUILDER_2D.min_range = 0.2
-TRAJECTORY_BUILDER_2D.max_range = 5.8
+TRAJECTORY_BUILDER_2D.max_range = 9.8
 TRAJECTORY_BUILDER_2D.missing_data_ray_length = 1.
 TRAJECTORY_BUILDER_2D.use_imu_data = false
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
