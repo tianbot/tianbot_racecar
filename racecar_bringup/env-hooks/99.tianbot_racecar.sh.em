04a470615f6d01470f5f3ea8b3cfff68c1305e03
Tian Bo <tianb03@hotmail.com>
2018-12-18 18:42:07 +0800

    racecar core now conform to new firmware. update config file name

--- racecar_bringup/env-hooks/99.tianbot_racecar.sh.em
+++ racecar_bringup/env-hooks/99.tianbot_racecar.sh.em
@@ -1,10 +1,10 @@
 # Set some defaults to tianbot racecar launch environment
 
-: ${TIANBOT_RACECAR_BASE:=compact}
-: ${TIANBOT_RACECAR_RGBD_CAMERA:=realsense_d435}
-: ${TIANBOT_RACECAR_LIDAR:=rplidar_a3}
-: ${TIANBOT_RACECAR_STEERING_REVERSE:=normal}
-: ${TIANBOT_RACECAR_THROTTLE_REVERSE:=normal}
+: ${TIANBOT_RACECAR_BASE:=compact}  #compact, standard, full.
+: ${TIANBOT_RACECAR_RGBD_CAMERA:=realsense_d435} #realsense_d415, realsense_d435, astra, astra_pro, xtion 
+: ${TIANBOT_RACECAR_LIDAR:=rplidar_a3} # rplidar_a1, rplidar_a2, rplidar_a3
+: ${TIANBOT_RACECAR_STEERING_REVERSE:=normal} # normal, reverse
+: ${TIANBOT_RACECAR_THROTTLE_REVERSE:=normal} # normal, reverse
 
 
 #Exports
--- racecar_core/script/tianbot_racecar_node.py
+++ racecar_core/script/tianbot_racecar_node.py
@@ -22,7 +22,7 @@ class TianBot_RACECAR_Class(object):
 
 #######################################################################################################################
         #Sensor variables
-        self._Counter = 0
+        self._counter = 0
 
         self._battery_value = 0
 
@@ -31,8 +31,9 @@ class TianBot_RACECAR_Class(object):
         self._qz = 0
         self._qw = 0
 
-        self._servo = 0
-        self._motor = 0
+        self._servo = 90
+        self._motor = 1500
+        self._speed_message = 's %d %d\r' %(int(self._servo),int(self._motor))
 
         self._robot_heading = 0
 #######################################################################################################################
@@ -44,7 +45,7 @@ class TianBot_RACECAR_Class(object):
 #######################################################################################################################
         rospy.loginfo("Starting with serial port: " + port + ", baud rate: " + str(baudRate))
         #Initializing SerialDataGateway with port, baudrate and callback function to handle serial data
-        self._SerialDataGateway = SerialDataGateway(port, baudRate,  self._HandleReceivedLine)
+        self._SerialDataGateway = SerialDataGateway(port, baudRate,  self.handle_received_line)
         rospy.loginfo("Started serial communication")
         
 
@@ -52,7 +53,7 @@ class TianBot_RACECAR_Class(object):
 #Subscribers and Publishers
 
         #Publisher for Battery level(for upgrade purpose)
-        self._Battery_Level = rospy.Publisher('battery_level',Float32,queue_size = 10)
+        self._battery_pub = rospy.Publisher('battery_level',Float32,queue_size = 10)
 
         #Publisher for entire serial data
         self._SerialPublisher = rospy.Publisher('serial', String,queue_size=10)
@@ -60,68 +61,74 @@ class TianBot_RACECAR_Class(object):
 #######################################################################################################################
 #Subscribers and Publishers of IMU data topic
 
-        self.frame_id = 'IMU_link'
+        self._frame_id = 'IMU_link'
 
-        self.cal_offset = 0.0
-        self.orientation = 0.0
-        self.cal_buffer =[]
-        self.cal_buffer_length = 1000
-        self.imu_data = Imu(header=rospy.Header(frame_id="IMU_link"))
-        self.imu_data.orientation_covariance = [0.0025,0,0,0,0.0025,0,0,0,0.0025]
-        self.imu_data.angular_velocity_covariance = [0.02,0,0,0,0.02,0,0,0,0.02]
-        self.imu_data.linear_acceleration_covariance = [0.04,0,0,0,0.04,0,0,0,0.04]
-        self.gyro_measurement_range = 300.0 
-        self.gyro_scale_correction = 1.35
-        self.imu_pub = rospy.Publisher('imu_data', Imu,queue_size = 10)
+        self._cal_offset = 0.0
+        self._orientation = 0.0
+        self._cal_buffer =[]
+        self._cal_buffer_length = 1000
+        self._imu_data = Imu(header=rospy.Header(frame_id="IMU_link"))
+        self._imu_data.orientation_covariance = [0.0025,0,0,0,0.0025,0,0,0,0.0025]
+        self._imu_data.angular_velocity_covariance = [0.02,0,0,0,0.02,0,0,0,0.02]
+        self._imu_data.linear_acceleration_covariance = [0.04,0,0,0,0.04,0,0,0,0.04]
+        self._gyro_measurement_range = 300.0 
+        self._gyro_scale_correction = 1.35
+        self._imu_pub = rospy.Publisher('imu_data', Imu,queue_size = 10)
 
-        self.deltat = 0
-        self.lastUpdate = 0
+        self._deltat = 0
+        self._last_update = 0
 
 #New addon for computing quaternion
         
-        self.pi = 3.1415926535
-        self.GyroMeasError = float(self.pi * ( 40 / 180 ))
-        self.beta = float(math.sqrt(3 / 4) * self.GyroMeasError)
+        self._pi = 3.1415926535
+        self._gyro_meas_error = float(self._pi * ( 40 / 180 ))
+        self._beta = float(math.sqrt(3 / 4) * self._gyro_meas_error)
 
-        self.GyroMeasDrift = float(self.pi * ( 2 / 180 ))
-        self.zeta = float(math.sqrt(3 / 4) * self.GyroMeasDrift)
+        self._gyro_meas_drift = float(self._pi * ( 2 / 180 ))
+        self._zeta = float(math.sqrt(3 / 4) * self._gyro_meas_drift)
 
 
-        self.beta = math.sqrt(3 / 4) * self.GyroMeasError
+        self._beta = math.sqrt(3 / 4) * self._gyro_meas_error
 
-        self.q = [1,0,0,0]
+        self._q = [1,0,0,0]
 #######################################################################################################################
 #Speed subscriber
-        self.motor_speed = rospy.Subscriber('/car/cmd_vel',Twist,self._Update_Speed)
+        self._speed_sub = rospy.Subscriber('/car/cmd_vel',Twist,self.update_speed)
 
+########################################################
+# Timer for serial communication
+
+        self._timer = rospy.Timer(rospy.Duration(0.05), self.car_controller)
 #######################################################################################################################
-    def _Update_Speed(self, speed):
+    def update_speed(self, speed):
 
         self._motor = 1500 + self._motor_direction*(speed.linear.x - 1500)
         self._servo = 90 + self._servo_direction*(speed.angular.z - 90)
 
-        speed_message = 's %d %d\r' %(int(self._servo),int(self._motor))
+        self._speed_message = 's %d %d\r' %(int(self._servo),int(self._motor))
 
-        self._WriteSerial(speed_message)
+#####################################
+    def car_controller(self, tdat):
+        self.write_serial(self._speed_message)
 
 #######################################################################################################################
 #Calculate orientation from accelerometer and gyrometer
 
-    def _HandleReceivedLine(self,  line):
+    def handle_received_line(self,  line):
 
-        self._Counter = self._Counter + 1
-        self._SerialPublisher.publish(String(str(self._Counter) + ", in:  " + line))
+        self._counter = self._counter + 1
+        self._SerialPublisher.publish(String(str(self._counter) + ", in:  " + line))
 
         if(len(line) > 0):
 
             lineParts = line.split('\t')
-            try:
 
+            try:
                 if(lineParts[0] == 'b'):
                     self._battery_value = float(lineParts[1])
 
 #######################################################################################################################
-                    self._Battery_Level.publish(self._battery_value)
+                    self._battery_pub.publish(self._battery_value)
 
 #######################################################################################################################
             
@@ -143,13 +150,13 @@ class TianBot_RACECAR_Class(object):
                     imu_msg = Imu()
                     h = Header()
                     h.stamp = rospy.Time.now()
-                    h.frame_id = self.frame_id
+                    h.frame_id = self._frame_id
 
                     imu_msg.header = h
 
-                    imu_msg.orientation_covariance = self.imu_data.orientation_covariance   
-                    imu_msg.angular_velocity_covariance = self.imu_data.angular_velocity_covariance
-                    imu_msg.linear_acceleration_covariance = self.imu_data.linear_acceleration_covariance 
+                    imu_msg.orientation_covariance = self._imu_data.orientation_covariance   
+                    imu_msg.angular_velocity_covariance = self._imu_data.angular_velocity_covariance
+                    imu_msg.linear_acceleration_covariance = self._imu_data.linear_acceleration_covariance 
 
                     imu_msg.orientation.x = self._qx
                     imu_msg.orientation.y = self._qy
@@ -164,7 +171,7 @@ class TianBot_RACECAR_Class(object):
                     #q_rot = quaternion_from_euler(self.pi, -self.pi/2, 0)
                     #q_ori = Quaternion(self._qx, self._qy, self._qz, self._qw)
                     #imu_msg.orientation = quaternion_multiply(q_ori, q_rot)
-                    self.imu_pub.publish(imu_msg)
+                    self._imu_pub.publish(imu_msg)
 
             except:
                 rospy.logwarn("Error in Sensor values")
@@ -175,14 +182,14 @@ class TianBot_RACECAR_Class(object):
 #######################################################################################################################
 
 
-    def _WriteSerial(self, message):
-        self._SerialPublisher.publish(String(str(self._Counter) + ", out: " + message))
+    def write_serial(self, message):
+        self._SerialPublisher.publish(String(str(self._counter) + ", out: " + message))
         self._SerialDataGateway.Write(message)
 
 #######################################################################################################################
 
 
-    def Start(self):
+    def start(self):
         rospy.logdebug("Starting")
         self._SerialDataGateway.Start()
 
@@ -207,7 +214,7 @@ if __name__ =='__main__':
     TianBot_RACECAR = TianBot_RACECAR_Class()
     try:
         
-        TianBot_RACECAR.Start()
+        TianBot_RACECAR.start()
         rospy.spin()
     except rospy.ROSInterruptException:
         rospy.logwarn("Error in main function")
