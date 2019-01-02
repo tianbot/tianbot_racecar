c9557ac88390b068ef3a823b6f2bc8f293b2ca00
tianb03 <tianb03@hotmail.com>
2019-01-02 17:43:47 +0800

    export ip explicitly in bringup.sh

--- racecar_bringup/install/_bin_/tianbot_racecar_bringup.sh
+++ racecar_bringup/install/_bin_/tianbot_racecar_bringup.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-# IP=$(ip addr show eth0 | grep -w inet | awk '{print $2}' | awk -F / '{print $1}')
+IP=$(ip addr show eth0 | grep -w inet | awk '{print $2}' | awk -F / '{print $1}')
 
 source /home/tianbot/tianbot_ws/devel/setup.bash
 
 export ROS_MASTER_URI=http://localhost:11311
 
-# export ROS_IP=${IP}
+export ROS_IP=${IP}
 
 roslaunch racecar_bringup racecar_bringup.launch
