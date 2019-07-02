346e8da4e24f9a26812107d8eabf14c74b55103e
Maxwell <wangjintai@tianbot.com>
2019-07-02 14:32:32 +0800

    device rules updated. minor bug fixed

--- racecar_bringup/install/_bin_/tianbot_racecar_core.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-IP=$(ip addr show eth0 | grep -w inet | awk '{print $2}' | awk -F / '{print $1}')
-source /home/tianbot/racecar_ws/devel/setup.bash
-export ROS_MASTER_URI=http://localhost:11311
-export ROS_IP=${IP}
-roslaunch racecar_core racecar_core.launch
